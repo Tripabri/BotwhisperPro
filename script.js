6e9003f82d8dd89de5728966c19b5c2757de6c77
Tripabri <114739878+Tripabri@users.noreply.github.com>
2023-09-02 01:02:42 +0200

    first web app

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scrolling for navigation links
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
